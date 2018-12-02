48d3ac3feb51e7608a385e3e639b3dd69a2d61dd
NoumanAffaq <noumanaffaq@gmail.com>
2018-12-02 18:02:10 +0500

    added bitch

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhoneBookApplication.Startup))]
+namespace PhoneBookApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
